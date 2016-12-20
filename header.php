1eb5c1e0d8ba85bcb995914a1d1c53203e036b73
Panhara28 <titpanhara@gmail.com>
2016-12-21 03:41:39 +0700

    commit

--- header.php
+++ header.php
@@ -1,1 +1,6 @@
 <h1>This the page</h1>
+<a href="index.php">index</a>
+<a href="anotherpage.php">anotherpage</a><br>
+<?php
+  $events = ["Bugsmash","Kata Camp","Code And Coffee"];
+ ?>
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
-
-
+  include 'header.php';
+  for ($i=0; $i <count($events) ; $i++) {
+    echo "<a href='#'>$events[$i]</a>"."<br>";
+  }
  ?>
