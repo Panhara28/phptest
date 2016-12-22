bb0a94e959d0e064af8160f4f31ff9ca756e25ca
Panhara28 <titpanhara@gmail.com>
2016-12-23 01:02:52 +0700

    commited

--- form.php
+++ form.php
@@ -1,40 +1,26 @@
-<?php include 'function.php'; ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" href="css/foundation.css">
   </head>
   <body>
-    <div class="row">
-        <div class="large-3 columns">
-            <nav>
-              <ul class="menu">
-                <li><a href="form.php">Form</a></li>
-                <li><a href="tamplate.php">Tamplate</a></li>
-              </ul>
-            </nav>
-        </div>
-    </div>
 
-    <div class="row">
-          <form class="large-12 cloumns" action="tamplate.php" method="post">
-            <div class="large-4 columns">
-              <input type="number" name="DC" placeholder="Data Communication"><br><br>
-              <input type="number" name="CA" placeholder="Computer Achitecture"><br><br>
-              <input type="number" name="DB" placeholder="Database"><br><br>
-            </div>
-            <div class="large-4 columns">
-              <input type="number" name="ENG" placeholder="English"><br><br>
-              <input type="number" name="CPP" placeholder="Cplusplus"><br><br>
-              <input type="number" name="DS" placeholder="Data Structure"><br><br>
-            </div>
-            <div class="large-4 columns">
-              <input class="button" type="submit" name="submit" value="Submit">
-            </div>
-          </form>
-    </div>
+    <?php include 'function.php'; Validate(); ?>
+    <form class="" action="" method="post">
+      <input type="number" name="math" placeholder="Math"><br><br>
+      <input type="number" name="cpp" placeholder="Cpp"><br><br>
+      <input type="number" name="fund" placeholder="Fund"><br><br>
+      <input type="submit" name="submit" value="Submit"><br>
+    </form>
 
+    <table>
+      <tr>
+        <?php echo subjectList(); ?>
+      </tr>
+      <tr>
+        <?php echo result(); ?>
+      </tr>
+    </table>
   </body>
 </html>
--- function.php
+++ function.php
@@ -1,12 +1,72 @@
-<link rel="stylesheet" href="css/foundation.css">
-  <?php
-    include 'partialize.php';
+<?php
+function subjectList(){
+  $array = array(
+    "Math",
+    "CPP",
+    "Fund",
+    "Average",
+    "Grade"
+  );
+  for ($i=0; $i <count($array) ; $i++) {
+    echo "<th>$array[$i]</th>";
+  }
+}
+
+  function Average($m,$cpp,$f){
+    $m = $_POST['math'];
+    $cpp = $_POST['cpp'];
+    $f = $_POST['fund'];
+    if (isset($_POST['submit'])){
+      $result = ($m+$cpp+$f)/3;
+      return $result;
+    }
+  }
+
+  function Grade(){
+    if (Average() > 90) {
+      $grade = ' A';
+    }elseif(Average() >= 90 || Average()>80){
+      $grade = ' B';
+    }elseif(Average()>=80 || Average()>70){
+      $grade = ' C';
+    }elseif(Average()>=70 || Average()>60){
+      $grade = ' D';
+    }elseif(Average()>=60 || Average()>=50){
+      $grade = ' E';
+    }else{
+      $grade = 'F';
+    }
     if (isset($_POST['submit'])) {
-      if (empty($_POST['DC']&&$_POST['CA']&&$_POST['DB']&&$_POST['ENG']&&$_POST['CPP']&&$_POST['DS'])) {
-        echo '<a style="color:red;" href="form.php">some field are empty click here to field again!</a>';
-      }else{
-        Average();
-        Grade();
+      return $grade;
+    }else{
+      echo ' ';
+    }
+  }
+
+  function result(){
+    $array = array(
+      $_POST['math'],
+      $_POST['cpp'],
+      $_POST['fund'],
+      Average(),
+      Grade()
+    );
+    for ($i=0; $i <count($array) ; $i++) {
+      if (empty($_POST['math']&&$_POST['fund']&&$_POST['cpp'])) {
+        echo ' ';
+      }else {
+        echo "<th>$array[$i]</th>";
       }
     }
-  ?>
+  }
+
+  function Validate(){
+    if (isset($_POST['submit'])) {
+      if (empty($_POST['math'] && $_POST['fund'] && $_POST['cpp'])) {
+        echo "All field *require";
+      }else{
+        Average();
+      }
+    }
+  }
+ ?>
